f6ebb20598dbe5ca9422f4fc55a15575ffd97177
Eric Zhai (----) <zhai65@imec.be>
2020-05-02 10:21:13 +0200

    complete two functions in cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To avoid repetive inverse calculation of large matrix, the two functions below intend to reuse cached
+##  inversion matrix results
 
-## Write a short comment describing this function
+## creates a special invertible matrix object returning a list of four functions to set, get, inverse(setsolve) a matrix and retrieve(getsovle) its inversion 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
+    
 
 
-## Write a short comment describing this function
+## calculates the inverse of the matrix created by the makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached inverse matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
